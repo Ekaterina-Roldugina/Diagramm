396335d8a98e05f40c420a9374f513d5ab1a1c31
Ekaterina-Roldugina <123230643+Ekaterina-Roldugina@users.noreply.github.com>
2023-12-27 19:54:16 +0300

    Add files via upload

---
+++ routers.py
@@ -0,0 +1,6 @@
+from tables.artifacts import router as artifacts_router
+from tables.flora_and_fauna import router as fauna_router
+from tables.pictures import router as pictures_router
+from tables.archeologists import router as archaeologists_router
+from tables.excavations import router as excavations_router
+from tables.investigations import router as investigations_router
